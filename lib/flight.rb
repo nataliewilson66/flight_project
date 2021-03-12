fec0e37fd7b25f548a457515256616666332d367
nataliewilson66 <nwilson@alumni.scu.edu>
2021-03-12 14:52:41 -0800

    Flight Project

---
+++ lib/flight.rb
@@ -0,0 +1,38 @@
+require_relative "passenger"
+
+class Flight
+
+  attr_reader :passengers
+
+  def initialize(flight_number, capacity)
+    @flight_number = flight_number
+    @capacity = capacity
+    @passengers = []
+  end
+
+  def full?
+    return true if @passengers.length == @capacity
+    false
+  end
+
+  def board_passenger(passenger)
+    if !self.full?
+      @passengers << passenger if passenger.has_flight?(@flight_number)
+    end
+  end
+
+  def list_passengers
+    names = []
+    @passengers.each { |p| names << p.name }
+    names
+  end
+
+  def [](index)
+    @passengers[index]
+  end
+
+  def <<(passenger)
+    board_passenger(passenger)
+  end
+
+end
